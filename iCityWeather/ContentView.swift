4c06221bdbce1a41acb88ec72ee443b2af14d587
chr1s78 <ayahirano2014@126.com>
2021-07-09 20:32:07 +0800

    Initial Commit

---
+++ iCityWeather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iCityWeather
+//
+//  Created by 吕博 on 2021/7/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iCityWeather/iCityWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCityWeatherApp.swift
+//  iCityWeather
+//
+//  Created by 吕博 on 2021/7/9.
+//
+
+import SwiftUI
+
+@main
+struct iCityWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
